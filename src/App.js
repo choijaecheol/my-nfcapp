c06d8f9677d301288f2e5a3eea5ffca1fd3f3a71
Choijaechul <choijaechul@gmail.com>
2023-04-07 15:30:21 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,65 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import Web3 from 'web3';
-
+import React from 'react';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Board from './pages/Board';
+import Test1 from './pages/Test1';
+import Test2 from './pages/Test2';
 
 function App() {
-  const [balance, setBalance] = useState(0); //balance 변수 초기값 0
-  let [isConnect, setIsConnect] = useState(false);
-  let [account, setAccount] = useState(''); //balance 변수 초기값 0
+    return (
+        <Router>
+            <Navbar bg="primary" expand='sm' className="mb-3">
+              <Container fluid>
+                <Navbar.Brand href="/">NFT Market</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse>
+                  <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/">홈</Nav.Link>
+                    <Nav.Link as={Link} to="/profile">profile</Nav.Link>
+                    <Nav.Link as={Link} to="/board">board</Nav.Link>
+                    <Nav.Link as={Link} to="/test1">test1</Nav.Link>
+                    <Nav.Link as={Link} to="/test2">test2</Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
 
-  // MetaMask를 통해 사용자에게 승인을 요청합니다.
-  // 승인 요청 버튼을 클릭하면 requestAccounts 함수를 실행합니다.
-  const requestAccounts = async () => {
-    try {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      console.log('Accounts: ', accounts);
-      setIsConnect(true) //이미 접속되어 있으면 true 로 변경.
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  //useEffect( ()=>{}, [] ) 이 때는 화면이 처음 나타났을 때 한 번만 실행된다.  
-  useEffect(() => {
-    async function getBalance() {
-      //web3 객체 생성.
-      const web3 = new Web3(
-        window.ethereum || 'http://localhost:8545'
-      );
-
-      // Metamask 라이브러리가 설치되어 있는지 확인합니다.
-      if (typeof window.ethereum !== 'undefined') {
-        console.log('MetaMask is installed!');
-      }
-
-      
-      //현재 계정을 가져오기. 
-      const accounts = await web3.eth.getAccounts();
-      const currentAccount = accounts[0];
-      console.log('currentAccount : ' + currentAccount);
-
-      if (typeof currentAccount !== 'undefined') {      
-          setIsConnect(true) //이미 접속되어 있으면 true 로 변경.
-          const weiBalance = await web3.eth.getBalance(currentAccount);
-          const etherBalance = web3.utils.fromWei(weiBalance, 'ether');
-          setBalance(etherBalance); //이더리움 잔액세팅.
-          setAccount(currentAccount); //계정 세팅.
-      }  
-      
-      
-    }
-    getBalance();
-  }, []);
-
-  return (
-    <div>
-      {!isConnect && (<button onClick={requestAccounts}> try metamask connect </button>)}
-      
-      {isConnect && (<p>안녕하세요 {account} 님. 현재 계정의 잔액: {balance} ETH</p>)}
-      
-    </div>
-  );
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/profile" element={<Profile />} />
+              <Route exact path="/board" element={<Board />} />
+              <Route exact path="/test1" element={<Test1 />}  />
+              <Route exact path="/test2" element={<Test2 />}  />
+            </Routes>
+            
+        </Router>
+    );
 }
 
 export default App;
-
---
+++ src/pages/Test2.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
+import MyNFT from './MyNFT.json';
+
+const Test2 = () => {
+    const [account, setAccount] = useState(null);
+  const [nftContract, setNftContract] = useState(null);
+  const [nfts, setNfts] = useState([]);
+
+  useEffect(() => {
+    async function connectWallet() {
+      if (!window.ethereum) {
+        alert('이더리움 지갑을 설치해주세요.');
+        return;
+      }
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0]);
+      } catch (err) {
+        console.log('이더리움 지갑에 로그인해주세요.');
+      }
+    }
+    connectWallet();
+  }, []);
+
+  useEffect(() => {
+    async function loadContract() {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = MyNFT.networks[networkId]; //error 위치...
+      const contract = new web3.eth.Contract(MyNFT.abi, deployedNetwork.address);
+      setNftContract(contract);
+    }
+    loadContract();
+  }, []);
+
+  useEffect(() => {
+    async function getNfts() {
+      if (nftContract) {
+        const nftCount = await nftContract.methods.totalSupply().call();
+        const nftList = [];
+        for (let i = 1; i <= nftCount; i++) {
+          const nftId = await nftContract.methods.tokenByIndex(i - 1).call();
+          const nft = await nftContract.methods.getNft(nftId).call();
+          nftList.push(nft);
+        }
+        setNfts(nftList);
+      }
+    }
+    getNfts();
+  }, [nftContract]);
+
+  async function buyNft(nft) {
+    if (nftContract) {
+      const price = nft.price;
+      const tx = {
+        from: account,
+        to: nftContract.options.address,
+        value: price,
+      };
+      await window.ethereum.request({ method: 'eth_sendTransaction', params: [tx] });
+      const txHash = await nftContract.methods.buyNft(nft.id).send({ from: account });
+      console.log(`트랜잭션 해시: ${txHash.transactionHash}`);
+    }
+  }
+
+  return (
+    <div>
+      <h1>NFT 거래 사이트</h1>
+      <div>계정: {account}</div>
+      <ul>
+        {nfts.map(nft => (
+          <li key={nft.id}>
+            <img src={nft.image} alt={nft.name} width={200} />
+            <h3>{nft.name}</h3>
+            <p>{nft.description}</p>
+            <p>{Web3.utils.fromWei(nft.price, 'ether')} ETH</p>
+            <button onClick={() => buyNft(nft)}>구매하기</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Test2;
